0a8ec57b7205d757127f9321fd6355809e764104
veronikageidt <prokopchuk_veron@mail.ru>
2022-01-06 06:48:06 +0500

    add all allowed hosts

--- joinup/settings.py
+++ joinup/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-5@#stj*s5vv+-e9nsqh#hg%8r6k!y*zj+6phu++s)=ng5c_4%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
